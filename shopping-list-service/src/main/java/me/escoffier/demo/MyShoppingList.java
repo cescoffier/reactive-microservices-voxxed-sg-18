8b051fc4867add113db5e5b55321c18bddc9c7e8
Clement Escoffier <clement.escoffier@gmail.com>
2018-05-31 14:50:08 +0800

    fix indentation

--- shopping-list-service/src/main/java/me/escoffier/demo/MyShoppingList.java
+++ shopping-list-service/src/main/java/me/escoffier/demo/MyShoppingList.java
@@ -1,8 +1,6 @@
 package me.escoffier.demo;
 
-import io.reactivex.Flowable;
 import io.reactivex.Single;
-import io.vertx.circuitbreaker.CircuitBreakerOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.circuitbreaker.CircuitBreaker;
 import io.vertx.reactivex.core.AbstractVerticle;
@@ -12,37 +10,36 @@ import io.vertx.reactivex.ext.web.RoutingContext;
 import io.vertx.reactivex.ext.web.client.HttpResponse;
 import io.vertx.reactivex.ext.web.client.WebClient;
 import io.vertx.reactivex.servicediscovery.ServiceDiscovery;
-import io.vertx.reactivex.servicediscovery.types.HttpEndpoint;
 
 public class MyShoppingList extends AbstractVerticle {
 
-    private WebClient shopping, pricer;
-    private CircuitBreaker circuit;
+  private WebClient shopping, pricer;
+  private CircuitBreaker circuit;
 
-    @Override
-    public void start() {
+  @Override
+  public void start() {
 
-        Router router = Router.router(vertx);
-        router.route("/health").handler(rc -> rc.response().end("OK"));
-        router.route("/").handler(this::getShoppingList);
+    Router router = Router.router(vertx);
+    router.route("/health").handler(rc -> rc.response().end("OK"));
+    router.route("/").handler(this::getShoppingList);
 
-        ServiceDiscovery.create(vertx, discovery -> {
-            // Get pricer-service
+    ServiceDiscovery.create(vertx, discovery -> {
+      // Get pricer-service
 
-            // Get shopping-backend
+      // Get shopping-backend
 
-            // Assigned the field and start the HTTP server When both have been retrieved
-            
-            vertx.createHttpServer()
-                .requestHandler(router::accept)
-                .listen(8080);
-        });
-        
-    }
+      // Assigned the field and start the HTTP server When both have been retrieved
 
-    private void getShoppingList(RoutingContext rc) {
-        HttpServerResponse serverResponse =
-            rc.response().setChunked(true);
+      vertx.createHttpServer()
+        .requestHandler(router::accept)
+        .listen(8080);
+    });
+
+  }
+
+  private void getShoppingList(RoutingContext rc) {
+    HttpServerResponse serverResponse =
+      rc.response().setChunked(true);
 
        /*
          +--> Retrieve shopping list
@@ -54,9 +51,9 @@ public class MyShoppingList extends AbstractVerticle {
                           write it to the HTTP response
          */
 
-        Single<JsonObject> single = shopping.get("/shopping")
-            .rxSend()
-            .map(HttpResponse::bodyAsJsonObject);
+    Single<JsonObject> single = shopping.get("/shopping")
+      .rxSend()
+      .map(HttpResponse::bodyAsJsonObject);
        
          /*
                                shopping          pricer
@@ -78,8 +75,7 @@ public class MyShoppingList extends AbstractVerticle {
           end <--|
          */
 
-        
 
-    }
+  }
 
 }
